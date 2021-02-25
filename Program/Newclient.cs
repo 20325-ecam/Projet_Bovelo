478f65e19cb9b4071a32ccec3a7a8acc437e7a33
20325-ecam <75428443+20325-ecam@users.noreply.github.com>
2021-02-25 10:05:41 +0100

    form new client

---
+++ Program/Newclient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectBovelo
+{
+    public partial class Newclient : BoveloBaseForm
+    {
+        public Newclient(BoveloUser user)
+        {
+            this.user = user;
+            InitializeComponent();
+        }
+
+        private void newclient_Load(object sender, EventArgs e)
+        {
+            PageLayoutMaker.SetBasePageLayout(this);
+            PageLayoutMaker.CreateQuitButton(this);
+            PageLayoutMaker.CreateLogoutUserButton(this);
+            PageLayoutMaker.CreateHeader(this, DBConnection.loadImage(1), user);
+        }
+    }
+}
