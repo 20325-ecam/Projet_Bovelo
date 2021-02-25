09b2eb26ae216dfcd671e30f3e86c5248c0c158a
20325-ecam <75428443+20325-ecam@users.noreply.github.com>
2021-02-25 11:49:05 +0100

    test planning

---
+++ Program/Planning.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectBovelo
+{
+    public partial class Planning : Form
+    {
+        public Planning()
+        {
+            InitializeComponent();
+            this.SplitContainerAdv1.FixedPanel = Syncfusion.Windows.Forms.Tools.Enums.FixedPanel.Panel1;
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
